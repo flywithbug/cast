dba38fc13d2019941f157d08b30412c7b32b4ad2
flywithbug <369495368@qq.com>
2019-06-09 16:01:19 +0800

    a

---
+++ model_info.go
@@ -0,0 +1,41 @@
+package cast
+
+import (
+	"encoding/json"
+)
+
+/**
+ * @author  ori
+ * @date 2019-06-08 17:12
+ * @description: 属性信息
+ */
+type Attribute struct {
+	Name     string `json:"name"`
+	Type     string `json:"type"`
+	Notes    string `json:"notes"`
+	Alias    string `json:"alias"`
+	Hash     string `json:"hash"`
+	Required bool   `json:"required"`
+	Default  string `json:"default"`
+}
+
+/**
+ * @author  ori
+ * @date 2019-06-08 17:21
+ * @description: TODO
+ */
+type DataModel struct {
+	Name         string
+	Alias        string
+	Desc         string
+	CreateTime   string
+	StartVersion string
+	EndVersion   string
+	Owner        string
+	Attributes   []Attribute
+}
+
+func (d DataModel) ToJson() string {
+	js, _ := json.Marshal(d)
+	return string(js)
+}
