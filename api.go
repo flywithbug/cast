e7e6561655f979fb237f96de9f57c170e611246a
flywithbug <flywithbug@gmail.com>
2019-06-11 16:08:13 +0800

    a

--- api.go
+++ api.go
@@ -11,20 +11,20 @@ const (
 	ApiMethodTypeOptions
 )
 
-type ApiInfo struct {
-	Path          string        //请求路径
-	Method        ApiMethodType //请求类型  POST <GET <PUT < DELETE < OPTIONS < HEADER
-	Name          string        //接口名称
-	Alias         string        //别名(中文名)
-	StartVersion  string        //起始版本
-	EndVersion    string        //结束版本
-	Version       string        //接口版本号
-	Desc          string        //接口说明
-	UpdateTime    int           //最后更新时间
-	CreateTime    int           //创建时间
-	RequestHeader DataModel     //请求参数体
-	RequestBody   DataModel     //请求参数体
-	ResponseBody  DataModel     //返回数据体
+type Api struct {
+	Path          string            //请求路径
+	Method        ApiMethodType     //请求类型  POST <GET <PUT < DELETE < OPTIONS < HEADER
+	Name          string            //接口名称
+	Alias         string            //别名(中文名)
+	StartVersion  string            //起始版本
+	EndVersion    string            //结束版本
+	Version       string            //接口版本号
+	Desc          string            //接口说明
+	UpdateTime    int               //最后更新时间
+	CreateTime    int               //创建时间
+	RequestHeader map[string]string //请求参数体  //{Content-Type: application/json} need 	是
+	RequestBody   DataModel         //请求参数体
+	ResponseBody  DataModel         //返回数据体
 
 	Status bool //接口状态
 
