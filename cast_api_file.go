9470cb7687feca6a34374f0e4acefe0168d63e7b
flywithbug <flywithbug@gmail.com>
2019-06-18 12:00:15 +0800

    cast

--- cast_api_file.go
+++ cast_api_file.go
@@ -165,10 +165,5 @@ func (a *ObjectiveAPIFileModel) formApiMFileString() {
 }
 
 func CastApiObjective_C_H_M(model Api) *ObjectiveAPIFileModel {
-	obm := formObjectiveAPIFileModel(model)
-	if obm == nil {
-		return nil
-	}
-	obm.formContainerString()
-	return obm
+	return formObjectiveAPIFileModel(model)
 }
