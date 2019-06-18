f0bb9702c462afb2393d6de3e85108b0799375b0
flywithbug <flywithbug@gmail.com>
2019-06-18 11:50:11 +0800

    cast

--- cast_api_file.go
+++ cast_api_file.go
@@ -38,26 +38,27 @@ type ObjectiveAPIFileModel struct {
 	M          string
 }
 
-func formObjectiveAPIFileModel(aM Api) (apiFileM ObjectiveAPIFileModel) {
+func formObjectiveAPIFileModel(aM Api) (apiFileM *ObjectiveAPIFileModel) {
+	apiFileM = new(ObjectiveAPIFileModel)
 	apiFileM.ContainerModel = make(map[string]string)
 	apiFileM.Api = aM
 	i, ok := Cache().get(aM.ResponseModelName)
 	if !ok {
-		return
+		return nil
 	}
 	dM, ok := i.(DataModel)
 	if !ok {
-		return
+		return nil
 	}
 	apiFileM.DataModel = dM
 
 	i, ok = Cache().get(aM.ParameterName)
 	if !ok {
-		return
+		return nil
 	}
 	dM, ok = i.(DataModel)
 	if !ok {
-		return
+		return nil
 	}
 	apiFileM.ParaModel = dM
 	apiFileM.formContainerString()
@@ -163,7 +164,7 @@ func (a *ObjectiveAPIFileModel) formApiMFileString() {
 	a.M = str
 }
 
-func CastApiObjective_C_H_M(model Api) ObjectiveAPIFileModel {
+func CastApiObjective_C_H_M(model Api) *ObjectiveAPIFileModel {
 	obm := formObjectiveAPIFileModel(model)
 	obm.formContainerString()
 	return obm
