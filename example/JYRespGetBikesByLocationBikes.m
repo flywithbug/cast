544c2283250e67ff6f7230d09d30b0c96fcdbfc8
flywithbug <flywithbug@gmail.com>
2019-06-18 13:16:22 +0800

    cast

---
+++ example/JYRespGetBikesByLocationBikes.m
@@ -0,0 +1,22 @@
+//
+//  JYRespGetBikesByLocationBikes.h
+//  JYCastKit
+//
+//  Created by Flywithbug on 2019-06-18.
+//  Copyright © 2019 hellobike. All rights reserved.
+//
+	
+#import "JYRespGetBikesByLocationBikes.h"
+	
+@implementation JYRespGetBikesByLocationBikes
+	
++ (NSDictionary *)modelContainerPropertyGenericClass {
+	// value should be Class or Class name.
+	return @{@"list" : [JYRespGetBikesByLocation class]};
+}
+@end
+	
+	
+	
+	
+	
