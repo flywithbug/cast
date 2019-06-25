c7e470d691931e3eaa28470c22f646b4814c274a
flywithbug <flywithbug@gmail.com>
2019-06-25 16:12:29 +0800

    java model cast

--- template/api.m
+++ template/api.m
@@ -9,7 +9,7 @@
 		success:(nullable void (^)(NSURLSessionDataTask *_Nullable task, {*para*} * _Nullable responseObject))success
 		failure:(nullable void (^)(NSURLSessionDataTask * _Nullable task,   JYNetError *_Nullable error))failure{
 	param.action = @"{*para*}";
-	return [self mapi_post:[self moduleApiServer:type] parameters:param repClass:[{*para*} class] progress:uploadProgress success:success failure:failure];
+	return [self mapi_post_moduleType:type parameters:param repClass:[{*para*} class] progress:uploadProgress success:success failure:failure];
 }
 
 @end
